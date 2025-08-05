51fd065d86ea1593c59b19472660ed82f5c28992
Laboratório FIAP <labsfiap@fiap.com.br>
2025-08-04 22:34:48 -0300

    nobru apelao

--- Aula 1 - Arrays e Métodos de Array/Exercicio Metodos de Array/Metodos de Array/script.js
+++ Aula 1 - Arrays e Métodos de Array/Exercicio Metodos de Array/Metodos de Array/script.js
@@ -1,35 +1,40 @@
 function executarPush() {
   const array = ['🍎', '🍌'];
+  array.push('🍇')
   const resultado = array;
   document.getElementById('resultado-push').textContent = resultado;
 }
 
 function executarPop() {
   const array = ['🍎', '🍌', '🍇'];
+  array.pop()
   const resultado = array;
   document.getElementById('resultado-pop').textContent = resultado;
 }
 
 function executarShift() {
   const array = ['🍎', '🍌', '🍇'];
+  array.shift()
   const resultado = array;
   document.getElementById('resultado-shift').textContent = resultado;
 }
 
 function executarUnshift() {
   const array = ['🍌', '🍇'];
-  const resultado = 'Resultado da operação unshift';
+  array.unshift('🍓')
+  const resultado = array;
   document.getElementById('resultado-unshift').textContent = resultado;
 }
 
 function executarIncludes() {
   const array =  ['🍎', '🍌', '🍇'];
-  const resultado = array;
+  const resultado = array.includes('🥝');
   document.getElementById('resultado-includes').textContent = resultado;
 }
 
 function executarReverse() {
   const array = ['🍎', '🍌', '🍇'];
+  array.reverse()
   const resultado = array;
   document.getElementById('resultado-reverse').textContent = resultado;
 }
@@ -37,7 +42,8 @@ function executarReverse() {
 function executarSort() {
   const array = [3, 1, 4, 2];
   const array2 = ["Brasil", "Argentina", "Noruega", "Italia"];
-
+  array.sort()
+  array2.sort()
   const resultado = array;
   document.getElementById('resultado-sort').textContent = resultado;
   const resultado2 = array2;
@@ -46,12 +52,14 @@ function executarSort() {
 
 function executarSlice() {
   const array = ['🍎', '🍌', '🍇', '🍓'];
-  const resultado = array;
+  
+  const resultado = array.slice(1);
   document.getElementById('resultado-slice').textContent = resultado;
 }
 
 function executarSplice() {
   const array = ['🍎', '🍌', '🍇'];
+  array.splice(1,1)
   const resultado = array;
   document.getElementById('resultado-splice').textContent = resultado;
 }
@@ -59,13 +67,13 @@ function executarSplice() {
 
 function executarIndexOf() {
   const array = ['🍎', '🍌', '🍇'];
-  const resultado = array;
+  const resultado = array.indexOf('🍇');
   document.getElementById('resultado-indexof').textContent = resultado;
 }
 
 function executarJoin() {
   const array = ['🍎', '🍌', '🍇'];
-  const resultado = array;
+  const resultado = array.join("-");
   document.getElementById('resultado-join').textContent = resultado;
 }
 
@@ -73,55 +81,55 @@ function executarJoin() {
 
 function executarForEach() {
   const array = ['🍎', '🍌', '🍇'];
-  const resultado = 'Resultado da operação forEach';
+  const resultado = array.forEach(x => console.log(x))
   document.getElementById('resultado-foreach').textContent = resultado;
 }
 
 function executarMap() {
   const array = ['🍎', '🍌', '🍇'];
-  const resultado = 'Resultado da operação map';
+  const resultado = array.map((value) => { return value + " ✅"})
   document.getElementById('resultado-map').textContent = resultado;
 }
 
  function executarMapHTML() {
     const dispositivos = ['🎮', '🕹️', '💻'];
-    const resultado = 'Resultado da operação map inserindo HTML';
+    const resultado = dispositivos
     document.getElementById('resultado-map-html').innerHTML = resultado.join('<br>');
   }
 
 function executarFilter() {
   const array = ['🍎', '🍌', '🍇'];
-  const resultado = 'Resultado da operação filter';
+  const resultado = array.filter((value) => value == "🍎")
   document.getElementById('resultado-filter').textContent = resultado;
 }
 
 function executarFind() {
   const array =  ['🍎', '🍌', '🍇'];
-  const resultado = 'Resultado da operação find';
+  const resultado = array.find((value) => value == "🍇")
   document.getElementById('resultado-find').textContent = resultado;
 }
 
 function executarFindIndex() {
   const array =  ['🍎', '🍌', '🍇'];
-  const resultado = 'Resultado da operação findIndex';
+  const resultado = array.findIndex((value) => value == "🍇")
   document.getElementById('resultado-findIndex').textContent = resultado;
 }
 
 function executarReduce() {
   const array = [1, 2, 3, 4];
-  const resultado = 'Resultado da operação reduce';
+  const resultado = array.reduce((oldVal, val) => oldVal + val)
   document.getElementById('resultado-reduce').textContent = resultado;
 }
 
 function executarEvery() {
   const array = [10, 20, 30, 40];
-  const resultado = 'Resultado da operação every';
+  const resultado = array.every(x => x > 5)
   document.getElementById('resultado-every').textContent = resultado;
 }
 
 function executarSome() {
   const array = ['🍎', '🍌', '🥝'];
-  const resultado = 'Resultado da operação some';
+  const resultado = array.some(value => value == "🍌")
   document.getElementById('resultado-some').textContent = resultado;
 }
 
@@ -129,8 +137,21 @@ function executarSome() {
 
 function executarEncadeamento() {
   const jogos = ['Zelda', 'Mario', 'Elden Ring', 'Fortnite', 'Minecraft'];
-  const resultado = 'Resultado da operação encadeada';
+  const resultado = jogos
+  .filter(x => x.includes("e"))
+  .map(y => y.toLowerCase())
+  .sort()
+
   document.getElementById("resultado-encadeamento").textContent = resultado.join(', ') || 'Nenhum jogo encontrado';
 }
 
+
+function executarLimpezaFiltro(){
+  const array = [' maçã ', 'banana', ' kiwi ', 'laranja']
+  const resultado = array.map(x => x.trim())
+     .filter(x=> x != "kiwi" && x != "banana")
+     .map(x => x.toUpperCase())
+
+  document.getElementById("resultado-limpeza-filtro").textContent = resultado
+}
 
